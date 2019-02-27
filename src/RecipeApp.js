db762d8bc0b15b2a4c11dd3dd26a40d5aace0ebc
Ulf Swedin <ulf@swedin.nu>
2019-02-27 15:54:18 +0100

    Initial commit

---
+++ src/RecipeApp.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import Recipe from "./Recipe";
+import Navbar from "./Navbar";
+import RecipeList from "./RecipeList";
+
+import "./RecipeApp.css";
+
+class RecipeApp extends Component {
+  render() {
+    return (
+      <div className="app">
+        <Navbar />
+        <RecipeList />
+      </div>
+    );
+  }
+}
+
+export default RecipeApp;
